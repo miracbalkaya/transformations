d445b598761f96df406719ff8c1196a6a85e347f
mirac <miracbalkaya029@gmail.com>
2021-10-08 22:54:45 +0300

    Initial commit

---
+++ src/main/java/com/example/transformations/TransformationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.transformations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransformationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transformations/TransformationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transformations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
